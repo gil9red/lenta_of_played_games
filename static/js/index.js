4efad7d60e5442139487efe5aae906904056a4c2
ipetrash <ilya.petrash@inbox.ru>
2021-01-12 11:45:20 +0500

    Added static/js/index.js

---
+++ static/js/index.js
@@ -0,0 +1,112 @@
+$(function() {
+    // Подгрузка данных с сервера при раскрытии
+    $(".collapse.multi-collapse").on('show.bs.collapse', function(e) {
+        let collapse_el = $(this);
+        let loaded_body_el = collapse_el.find('.loaded-body');
+        if (loaded_body_el.length == 0) {
+            return;
+        }
+
+        let year = collapse_el.data('year');
+        $.get(
+            '/year/' + year,
+            function(data) {
+                loaded_body_el.html(data);
+                loaded_body_el.removeClass('loaded-body');
+
+                // Применение поиска на только что подгруженные игры
+                search(true, collapse_el.attr('id'));
+            }
+        );
+    })
+});
+
+function search(init=false, search_from_id=null) {
+    let search_text = $('#search').val().toLowerCase();
+    let is_empty = search_text.length == 0;
+
+    // Если функция вызвана при прогрузке страницы и текста в поиске нет,
+    // то ничего не делаем -- по умолчанию все элементы и так видимые
+    if (init && is_empty) {
+        return;
+    }
+
+    // При очистке строки поиска, убираем класс у всех не найденных ранее элементов
+    if (is_empty) {
+        $('.not_found').removeClass('not_found');
+    }
+
+    let query = '.media.game';
+    if (search_from_id != null) {
+        query = `#${search_from_id} ${query}`;
+    }
+
+    console.log(`Call search "${search_text}", using "${query}"`);
+
+    let day_by_games = new Map();
+
+    $(query).each(function() {
+        let game_el = $(this);
+        let day_el = game_el.parent().parent()
+
+        let text = game_el.text().replace(/\s\s+/g, ' ').trim().toLowerCase();
+        let is_visible = text.includes(search_text);
+        game_el.toggleClass('not_found', !is_visible);
+
+        // Если хоть одна игра видима, то показываем блок ее карточки
+        // Иначе, если уже был поиск и ее карточка была скрыта, то при проверке
+        // видимости игры она не будет показана, т.к. ее карточка скрыта
+        if (is_visible) {
+            day_el.removeClass('not_found');
+        }
+
+        let day_el_dom = day_el[0];
+        if (!day_by_games.has(day_el_dom)) {
+            day_by_games.set(day_el_dom, []);
+        }
+
+        day_by_games.get(day_el_dom).push(game_el);
+    });
+
+    for (let [day_el_dom, games] of day_by_games) {
+        let number_visibles = 0;
+        for (let game_el of games) {
+            if (!game_el.hasClass("not_found")) {
+                number_visibles++;
+            }
+        }
+        $(day_el_dom).toggleClass('not_found', number_visibles == 0);
+    }
+}
+
+function set_theme_color() {
+    if ($('#darkSwitch').is(':checked')){
+        $('meta[name=theme-color]').attr('content', '#333');
+    } else {
+        $('meta[name=theme-color]').attr('content', '#eee');
+    }
+}
+
+$(function() {
+    $('#clear_search').click(function() {
+        $('#search').val('');
+        search();
+    });
+
+    var typingTimer = null;       // Timer identifier
+    var doneTypingInterval = 300; // Time in ms
+
+    $('#search').on('input', function() {
+        clearTimeout(typingTimer);
+        typingTimer = setTimeout(search, doneTypingInterval);
+    });
+
+    search(true);
+
+    set_theme_color();
+
+    // Change theme-color from darkSwitch
+    $('#darkSwitch').click(function() {
+        set_theme_color();
+    });
+});
