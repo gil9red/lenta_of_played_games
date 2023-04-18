8a94e385d9af87c2fd33d04efc326fa377b9a0e2
gil9red <ilya.petrash@inbox.ru>
2023-04-18 14:00:41 +0500

    Refactoring. Using black code style

--- migrations/001.py
+++ migrations/001.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
 
 
 # SOURCE: http://docs.peewee-orm.com/en/latest/peewee/playhouse.html#schema-migrations
@@ -18,6 +18,6 @@ migrator = SqliteMigrator(db)
 with db.atomic():
     migrate(
         migrator.add_column(
-            'game', 'root_alias_id', ForeignKeyField(Game, Game.id, null=True)
+            "game", "root_alias_id", ForeignKeyField(Game, Game.id, null=True)
         ),
     )
