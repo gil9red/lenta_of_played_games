1ae6859a5c68ab5164c55ee1961378892665e4fe
gil9red <ilya.petrash@inbox.ru>
2023-10-16 18:32:13 +0500

    Updated UI. Bug fix on search

--- static/js/index.js
+++ static/js/index.js
@@ -1,3 +1,79 @@
+const not_found_class = "not_found";
+
+function search(init=false, search_from_id=null) {
+    let search_text = $('#search').val().toLowerCase();
+    let is_empty = search_text.length == 0;
+
+    // Если функция вызвана при прогрузке страницы и текста в поиске нет,
+    // то ничего не делаем -- по умолчанию все элементы и так видимые
+    if (init && is_empty) {
+        return;
+    }
+
+    // При очистке строки поиска, убираем класс у всех не найденных ранее элементов
+    if (is_empty) {
+        $('.' + not_found_class).removeClass(not_found_class);
+    }
+
+    let query = '.media.game';
+    if (search_from_id != null) {
+        query = `#${search_from_id} ${query}`;
+    }
+
+    console.log(`Call search "${search_text}", using "${query}"`);
+
+    let day_by_games = new Map();
+
+    $(query).each(function() {
+        let $game = $(this);
+        let $day = $game.parent().parent()
+        let day_value = $day.find('.day').text();
+
+        let text = day_value + $game.text().replace(/\s\s+/g, ' ').trim().toLowerCase();
+        let is_visible = text.includes(search_text);
+        $game.toggleClass(not_found_class, !is_visible);
+
+        // Если хоть одна игра видима, то показываем блок ее карточки
+        // Иначе, если уже был поиск и ее карточка была скрыта, то при проверке
+        // видимости игры она не будет показана, т.к. ее карточка скрыта
+        if (is_visible) {
+            $day.removeClass(not_found_class);
+        }
+
+        let day_el_dom = $day[0];
+        if (!day_by_games.has(day_el_dom)) {
+            day_by_games.set(day_el_dom, []);
+        }
+
+        day_by_games.get(day_el_dom).push($game);
+    });
+
+    for (let [day_el_dom, games] of day_by_games) {
+        let number_visibles = 0;
+        for (let $game of games) {
+            if (!$game.hasClass(not_found_class)) {
+                number_visibles++;
+            }
+        }
+        $(day_el_dom).toggleClass(not_found_class, number_visibles == 0);
+    }
+
+    $('.collapse[data-year]').each(function() {
+        let $collapse = $(this);
+        let is_all_not_found = true;
+
+        $collapse.find('.card-body > .media').each(function() {
+            let $day = $(this);
+            if (!$day.hasClass(not_found_class)) {
+                is_all_not_found = false;
+                return false;
+            }
+        });
+
+        $collapse.find('.no_results').toggleClass('hide', !is_all_not_found);
+    });
+}
+
 $(function() {
     // Загрузка остальных годов
     $(".card:has(.not-loaded)").each(function() {
@@ -61,76 +137,3 @@ $(function() {
     });
 });
 
-function search(init=false, search_from_id=null) {
-    let search_text = $('#search').val().toLowerCase();
-    let is_empty = search_text.length == 0;
-
-    // Если функция вызвана при прогрузке страницы и текста в поиске нет,
-    // то ничего не делаем -- по умолчанию все элементы и так видимые
-    if (init && is_empty) {
-        return;
-    }
-
-    // При очистке строки поиска, убираем класс у всех не найденных ранее элементов
-    if (is_empty) {
-        $('.not_found').removeClass('not_found');
-    }
-
-    let query = '.media.game';
-    if (search_from_id != null) {
-        query = `#${search_from_id} ${query}`;
-    }
-
-    console.log(`Call search "${search_text}", using "${query}"`);
-
-    let day_by_games = new Map();
-
-    $(query).each(function() {
-        let $game = $(this);
-        let $day = $game.parent().parent()
-        let day_value = $day.find('.day').text();
-
-        let text = day_value + $game.text().replace(/\s\s+/g, ' ').trim().toLowerCase();
-        let is_visible = text.includes(search_text);
-        $game.toggleClass('not_found', !is_visible);
-
-        // Если хоть одна игра видима, то показываем блок ее карточки
-        // Иначе, если уже был поиск и ее карточка была скрыта, то при проверке
-        // видимости игры она не будет показана, т.к. ее карточка скрыта
-        if (is_visible) {
-            $day.removeClass('not_found');
-        }
-
-        let day_el_dom = day_el[0];
-        if (!day_by_games.has(day_el_dom)) {
-            day_by_games.set(day_el_dom, []);
-        }
-
-        day_by_games.get(day_el_dom).push($game);
-    });
-
-    for (let [day_el_dom, games] of day_by_games) {
-        let number_visibles = 0;
-        for (let $game of games) {
-            if (!$game.hasClass("not_found")) {
-                number_visibles++;
-            }
-        }
-        $(day_el_dom).toggleClass('not_found', number_visibles == 0);
-    }
-
-    $('.collapse[data-year]').each(function() {
-        let $collapse = $(this);
-        let is_all_not_found = true;
-
-        $collapse.find('.card-body > .media').each(function() {
-            let day_el = $(this);
-            if (!day_el.hasClass('not_found')) {
-                is_all_not_found = false;
-                return false;
-            }
-        });
-
-        $collapse.find('.no_results').toggleClass('hide', !is_all_not_found);
-    });
-}
