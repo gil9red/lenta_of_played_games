bea869f39046e292b6d53bca920bb6db4104a3f9
gil9red <ilya.petrash@inbox.ru>
2025-06-15 20:14:41 +0500

    Рефакторинг. web.py

--- web.py
+++ web.py
@@ -5,8 +5,9 @@ __author__ = "ipetrash"
 
 
 import os.path
+from datetime import date
 
-from flask import Flask, render_template, send_from_directory, jsonify, session
+from flask import Flask, render_template, send_from_directory, session
 
 from flask_httpauth import HTTPBasicAuth
 from werkzeug.security import generate_password_hash, check_password_hash
@@ -20,15 +21,12 @@ from third_party.mini_played_games_parser import FINISHED_GAME, FINISHED_WATCHED
 log = get_logger("web", DIR_LOGS)
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = SECRET_KEY
+app.config["SECRET_KEY"] = SECRET_KEY
 
 auth = HTTPBasicAuth()
 
 
-USERS = {
-    login: generate_password_hash(password)
-    for login, password in users.items()
-}
+USERS = {login: generate_password_hash(password) for login, password in users.items()}
 
 
 @auth.verify_password
@@ -51,28 +49,28 @@ def verify_password(username: str, password: str) -> str | None:
 @app.route("/")
 @auth.login_required
 def index():
-    year_by_number = Game.get_year_by_number()
-    last_year = year_by_number[0][0]
+    year_by_number: list[tuple[int, int]] = Game.get_year_by_number()
+    last_year: int = year_by_number[0][0] if year_by_number else date.today().year
 
-    games = Game.get_all_finished(sort=False)
-    total_finished_game = len([game for game in games if game.category == FINISHED_GAME])
-    total_finished_watched = len([game for game in games if game.category == FINISHED_WATCHED])
+    games: list[Game] = Game.get_all_finished(sort=False)
+    total_finished_game: int = len(
+        [game for game in games if game.category == FINISHED_GAME]
+    )
+    total_finished_watched: int = len(
+        [game for game in games if game.category == FINISHED_WATCHED]
+    )
 
     return render_template(
         "index.html",
         title="Лента игр",
         year_by_number=year_by_number,
         day_by_games=Game.get_day_by_games(last_year),
-
         finished_game=FINISHED_GAME,
         finished_watched=FINISHED_WATCHED,
-
         finished_game_title="🎮 Пройденные",
         finished_watched_title="📺 Просмотренные",
-
         total_finished_game=total_finished_game,
         total_finished_watched=total_finished_watched,
-
         all_platforms=Game.get_platforms(),
     )
 
@@ -85,29 +83,19 @@ def year(year: int):
     )
 
 
-@app.route("/api/get_all_finished")
-def api_get_all_finished():
-    return jsonify([
-        dict(
-            name=game.name,
-            platform=game.platform,
-            category=game.category,
-            append_datetime=game.append_datetime_dt.isoformat() if game.append_datetime_dt else None,
-            finish_datetime=game.finish_datetime_dt.isoformat() if game.finish_datetime_dt else None,
-        )
-        for game in Game.get_all_finished()
-    ])
-
-
 @app.route("/favicon.ico")
 def favicon():
-    return send_from_directory(os.path.join(app.root_path, "static/img"), "favicon.png")
+    return send_from_directory(
+        os.path.join(app.root_path, "static/img"),
+        "favicon.png",
+    )
 
 
 if __name__ == "__main__":
     # app.debug = True
     if app.debug:
         import logging
+
         logging.basicConfig(level=logging.DEBUG)
 
     host = "0.0.0.0"
